e5a2068ad584c553bf0526973bd5d55dd1fe6dd0
Chris Osborne <chrisosbii@gmail.com>
2023-08-10 01:49:06 -0500

    updated routes

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+/*
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -11,5 +12,22 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
+
+module.exports = sequelize;*/
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findAll({
+      include: [
+        { model: Category, through: ProductTag }, 
+        { model: Tag, through: ProductTag }
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findByPk(req.params.id, {
+      include: [
+        { model: Category, through: ProductTag }, 
+        { model: Tag, through: ProductTag }
+      ],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -94,6 +120,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
